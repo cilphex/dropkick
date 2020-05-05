2c8b0e7ed6a0c061a967fbde5c80db66a3d65a54
Craig Hammell <cilphex@gmail.com>
2020-05-05 02:22:50 -0700

    add more stun urls

--- src/lib/globals.js
+++ src/lib/globals.js
@@ -5,7 +5,22 @@ const rtcPeerConnectionMeta = () => {
     iceServers: [
       {
         urls: [
-          'stun:stun.l.google.com:19302'
+          'stun:stun.l.google.com:19302',
+          'stun:stun.comtube.com:3478',
+          'stun:stun.comtube.ru:3478',
+          'stun:stun.cope.es:3478',
+          'stun:stun.counterpath.com:3478',
+          'stun:stun.counterpath.net:3478',
+          'stun:stun.cryptonit.net:3478',
+          'stun:stun.darioflaccovio.it:3478',
+          'stun:stun.datamanagement.it:3478',
+          'stun:stun.rolmail.net:3478',
+          'stun:stun.rounds.com:3478',
+          'stun:stun.rynga.com:3478',
+          'stun:stun.samsungsmartcam.com:3478',
+          'stun:stun.schlund.de:3478',
+          'stun:stun.services.mozilla.com:3478',
+          'stun:stun.sigmavoip.com:3478',
         ]
       }
     ]
